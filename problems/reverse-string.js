50c1577dd97a2d0423c151ef27d38f860615cf26
Babak <bgolsh2@gmail.com>
2023-08-31 15:15:36 -0700

    up to 3:15

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,13 @@
-module.exports = function reverseString(string) {
-  // Your code here
-};
+module.exports.reverseString = reverseString;
+
+function reverseString(string) {
+  let arr = [];
+  let char = string.split("");
+  for (let i = char.length - 1; i >= 0; i--) {
+    let ele = char[i];
+    arr.push(ele);
+  }
+  return arr.join("");
+}
+
+console.log(reverseString("hello"));
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,30 @@
-// Your code here
+const { reverseString } = require("../problems/reverse-string");
+const chai = require("chai");
+const expect = chai.expect;
+
+describe("reverseString(string)", () => {
+  let fun;
+  it("should reverse input string", () => {
+    const string1 = "fun";
+    expect(fun).to.equal("nuf");
+  });
+});
+
+// describe("isFive(num)", () => {
+//   it("should return true if the num is 5 otherwise false", () => {
+//     const num1 = 5;
+
+//     const actual1 = isFive(num1); // should be true
+
+//     expect(actual1).to.be.true;
+
+//     const otherInput = "not 5";
+//     const num2 = 4;
+
+//     const actual2 = isFive(num2); // should be false
+//     const actual3 = isFive(otherInput); // should be false
+
+//     expect(actual2).to.be.false;
+//     expect(actual3).to.be.false;
+//   });
+// });
