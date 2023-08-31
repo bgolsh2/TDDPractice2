f55c6c3624504d4c6810983ded3e56128dccc020
Babak <bgolsh2@gmail.com>
2023-08-31 16:08:11 -0700

    "omg"

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,25 +1,17 @@
-
 function reverseString(string) {
-  let newStr = '';
+  let newStr = "";
 
   for (let i = string.length - 1; i >= 0; i--) {
     let char = string[i];
     newStr += char;
-
   }
   return newStr;
 }
 
-
 console.log(reverseString("hello"));
-
-
-
-
 
 // __________________________________________________________________________________
 // DO NOT EDIT BELOW THS LINE
 // __________________________________________________________________________________
-
 
 module.exports = { reverseString };
